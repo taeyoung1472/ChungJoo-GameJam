f70a710b107891d749476b8767e4f03bc1c59608
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2022-08-20 08:49:15 +0900

    스포너

--- Assets/01.Script/Core/SpawnManager.cs
+++ Assets/01.Script/Core/SpawnManager.cs
@@ -4,5 +4,70 @@ using UnityEngine;
 
 public class SpawnManager : MonoSingleTon<SpawnManager>
 {
+    [SerializeField] private WavePerSpawn[] wavePerSpawnData;
+    private int curWave;
+    public void Start()
+    {
+        StartCoroutine(WaveSystem());
+        StartCoroutine(Spawn());
+    }
 
+    IEnumerator WaveSystem()
+    {
+        while (curWave < wavePerSpawnData.Length)
+        {
+            yield return new WaitForSeconds(30);
+            curWave++;
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(wavePerSpawnData[curWave].spawnDelay);
+            Enemy e = Instantiate(GenerateSpawnEnemy());
+        }
+    }
+
+    Enemy GenerateSpawnEnemy()
+    {
+        Enemy e = null;
+        int totalWeight = 0;
+        int tempWeight = 0;
+        int randIdx = 0;
+
+        foreach (var w in wavePerSpawnData[curWave].spawnWights)
+        {
+            totalWeight += w.weight;
+        }
+
+        randIdx = Random.Range(1, totalWeight + 1);
+
+        foreach (var w in wavePerSpawnData[curWave].spawnWights)
+        {
+            tempWeight += w.weight;
+            if(randIdx <= tempWeight)
+            {
+                e = w.spawnEnemy;
+                break;
+            }
+        }
+
+        return e;
+    }
+
+    [System.Serializable]
+    public class WavePerSpawn
+    {
+        public SpawnWight[] spawnWights;
+        public float spawnDelay;
+    }
+
+    [System.Serializable]
+    public class SpawnWight
+    {
+        public Enemy spawnEnemy;
+        public int weight = 1;
+    }
 }
