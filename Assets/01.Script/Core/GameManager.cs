4b07ff5513fef0dd5ba48eedaca9c095e70b21c2
taeyoung1472 <77605585+taeyoung1472@users.noreply.github.com>
2022-08-19 17:29:54 +0900

    최초푸시

---
+++ Assets/01.Script/Core/GameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoSingleTon<GameManager>
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/01.Script/Core/Json/JsonData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System;
+
+[Serializable]
+public class JsonData
+{
+
+}
